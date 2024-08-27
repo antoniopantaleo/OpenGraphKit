7a1864ecf1bfbad65343fb604ac7257919d73f18
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-08-27 15:35:30 +0200

    feat: use ImageRenderer to generate png image from a SwiftUI View

--- Package.swift
+++ Package.swift
@@ -1,20 +1,30 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OpenGraphKit",
+    platforms: [.macOS(.v13)],
+    products: [
+        .executable(
+            name: "opengraphkit",
+            targets: ["OpenGraphKit"]
+        )
+    ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "1.2.0"
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "OpenGraphKit",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                ),
             ]
         ),
     ]
--- Sources/OpenGraphKit.swift
+++ Sources/OpenGraphKit.swift
@@ -1,14 +1,45 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-// 
-// Swift Argument Parser
-// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
-
 import ArgumentParser
+import SwiftUI
 
 @main
-struct OpenGraphKit: ParsableCommand {
-    mutating func run() throws {
-        print("Hello, world!")
+struct OpenGraphKit: AsyncParsableCommand {
+    
+    @MainActor
+    mutating func run() async throws {
+        guard let scaleFactor =  NSScreen.main?.backingScaleFactor else { return }
+        let renderer = ImageRenderer(content: viewToSnapshot("Hello world"))
+        renderer.scale = scaleFactor
+        let image = renderer.cgImage
+        guard let data = image?.png else { return }
+        FileManager.default.createFile(atPath: "myImage.png", contents: data)
+        print("✅", "saved")
+    }
+    
+    func viewToSnapshot(_ title: String) -> some View {
+        VStack(spacing: 5) {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(title)
+        }
+        .frame(width: 1080, height: 720)
+        .background(Color.red)
+    }
+}
+
+fileprivate extension CGImage {
+    var png: Data? {
+        guard 
+            let mutableData = CFDataCreateMutable(nil, 0),
+            let destination = CGImageDestinationCreateWithData(
+                mutableData,
+                "public.png" as CFString,
+                1,
+                nil
+            )
+        else { return nil }
+        CGImageDestinationAddImage(destination, self, nil)
+        guard CGImageDestinationFinalize(destination) else { return nil }
+        return mutableData as Data
     }
 }
