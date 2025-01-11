d211cec27e27233eec765b5adb1071dc8a5fe4ca
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-01-11 19:29:59 +0100

    feat: upgrade to swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -19,10 +19,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "OpenGraphKit",
-            swiftSettings: [
-                .enableUpcomingFeature("BareSlashRegexLiterals")
-            ]
+            name: "OpenGraphKit"
         ),
         .executableTarget(
             name: "OpenGraphKit-Executable",
